41cb0af01ec897384023439fd84516c86b5c64ad
taroun <aksghk4671@gmail.com>
2021-12-10 16:16:52 +0900

    12/10 drf cerate api view

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate
+from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView
 
 app_name = 'accountapp'
 
@@ -10,6 +10,7 @@ urlpatterns = [
     # 로직 처리 위한 부분
     path('hello_world/', hello_world, name='hello_world'),
 
-    path('create/', AccountCreateTemplate, name='create_template'),
+    path('create_template/', AccountCreateTemplate, name='create_template'),
+    path('create/', AccountCreateAPIView.as_view(), name='create'),
 ]
 
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,13 +1,15 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.decorators import api_view
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from accountapp.models import NewModel
 
 
 # UI 설정부분
-from accountapp.serializers import NewModelSerializer
+from accountapp.serializers import NewModelSerializer, UserSerializer
 
 
 def hello_world_template(request):
@@ -38,3 +40,9 @@ def hello_world(request):
 
 def AccountCreateTemplate(request):
     return render(request, 'accountapp/create.html')
+
+
+class AccountCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = []
