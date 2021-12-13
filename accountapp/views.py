23c9190c13f05bb9f4b095c9349221ebcf8e708d
aksgh <aksghk4671@gmail.com>
2021-12-13 16:18:41 +0900

    12/13 Retrieve API view update

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import authentication, permissions
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import api_view
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,7 +13,7 @@ from accountapp.models import NewModel
 
 
 # UI 설정부분
-from accountapp.serializers import NewModelSerializer, UserSerializer
+from accountapp.serializers import NewModelSerializer, UserSerializer, UserWithoutPasswordSerializer
 
 
 def hello_world_template(request):
@@ -54,3 +55,10 @@ class AccountCreateAPIView(CreateAPIView):
 def AccountLoginView(request):
     return render(request, 'accountapp/login.html')
 
+
+class AccountRetrieveAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserWithoutPasswordSerializer
+
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = [TokenAuthentication]
