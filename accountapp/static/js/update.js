03bf4026f37b1b08269d8efae488cd95a2a8fcb1
aksgh <aksghk4671@gmail.com>
2021-12-14 15:13:01 +0900

    12/14 update API error code update

--- accountapp/static/js/update.js
+++ accountapp/static/js/update.js
@@ -29,8 +29,16 @@ function update_account(pk) {
       .catch(function (error) {
         // handle error
         console.log(error);
-        document.getElementById('alert_box').innerHTML
-             = "<div class='btn btn-danger rounded-pill px-5'>update가 실패 했습니</div>"
+        if (error.response.status === 401) {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>인증 정보가 없습니다.</div>"
+            } else if (error.response.status === 403) {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>권한이 없습니다.</div>"
+            } else {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>업데이트에 실패했습니다.</div>"
+            }
       })
       .then(function () {
         // always executed
