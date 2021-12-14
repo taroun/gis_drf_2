0b3ab88df5ae49bb2700ba13060c9e7d1aeea4e7
aksgh <aksghk4671@gmail.com>
2021-12-14 11:41:50 +0900

    12/14 Update API template javascript update 2

--- accountapp/static/js/update.js
+++ accountapp/static/js/update.js
@@ -16,3 +16,27 @@ function initialize (pk) {
         // always executed
       });
 }
+
+function update_account (pk) {
+    axios.patch('/accounts/update/' + pk , {
+        username: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+    })
+      .then(function (response) {
+        // handle success
+        console.log(response);
+
+        window.location.href = '/accounts/retrieve_template/' + pk ;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+
+        document.getElementById('alert_box').innerHTML
+             = "<div class='btn btn-danger rounded-pill px-5'>update가 실패 했습니</div>"
+
+      })
+      .then(function () {
+        // always executed
+      });
+}
