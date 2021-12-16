26c2a66cf66be1439343fa9d2a2b2a623af94d02
aksgh <aksghk4671@gmail.com>
2021-12-16 09:44:12 +0900

    12/16 profile template update0

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -5,7 +5,7 @@ from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView, Pr
 
 urlpatterns = [
     path('create_template/', ProfileCreateTemplateView.as_view(), name='create_template'),
-    path('update_template/', ProfileUpdateTemplateView.as_view(), name='update_template'),
+    path('update_template/<int:pk>', ProfileUpdateTemplateView.as_view(), name='update_template'),
 
     path('', ProfileCreateAPIView.as_view(), name='create'),
     path('<int:pk>', ProfileUpdateAPIView.as_view(), name='update'),
