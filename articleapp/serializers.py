ce606cc9e46ab6485001372c68edfe5f55980dd2
aksgh <aksghk4671@gmail.com>
2021-12-17 09:32:07 +0900

    12/16 article create view update

---
+++ articleapp/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+
+from articleapp.models import Article
+
+
+class ArticleSerializer(ModelSerializer):
+    class Meta:
+        model = Article
+        field = ['title', 'image', 'content']
+
